87d1ae56cb2167cf6a4e441c39d5c7da29851d23
London <londonsmith@stu.d214.org>
2023-04-06 14:30:47 -0500

    added AdjectiveView and SummaryView navigation link

---
+++ AdjectiveView.swift
@@ -0,0 +1,14 @@
+//
+//  AdjectiveView.swift
+//  GitHubPractice2
+//
+//  Created by London Smith on 4/6/23.
+//
+
+import SwiftUI
+
+struct AdjectiveView: View {
+    var body: some View {
+        NavigationLink("SummaryView go", destination: SummaryView())
+    }
+}
