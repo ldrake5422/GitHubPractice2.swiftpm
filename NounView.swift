005967247f274d717e77e3d1e168989c58287cd0
London <londonsmith@stu.d214.org>
2023-04-06 14:23:13 -0500

    added verbview navigation link

---
+++ NounView.swift
@@ -0,0 +1,14 @@
+//
+//  NounView.swift
+//  GitHubPractice2
+//
+//  Created by London Smith on 4/6/23.
+//
+
+import SwiftUI
+
+struct NounView: View {
+    var body: some View {
+        NavigationLink("VerbView go", destination: VerbView())
+    }
+}
