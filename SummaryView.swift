293b087d8e9477897741e38487550c027f64d93f
Lincoln Drake <ldrake5422@stu.d214.org>
2023-04-06 14:46:17 -0500

    Added Obama

---
+++ SummaryView.swift
@@ -0,0 +1,18 @@
+//
+//  SummaryView.swift
+//  GitHubPractice2
+//
+//  Created by Lincoln Drake on 4/6/23.
+//
+
+import SwiftUI
+
+struct SummaryView: View {
+    var body: some View {
+        
+        VStack {
+            
+            Text("Im Obama running very fast")
+        }
+    }
+}
