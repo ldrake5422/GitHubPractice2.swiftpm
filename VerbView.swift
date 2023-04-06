eb65f9b882be9de19d1a6c5d0a3cfb04d399c981
London <londonsmith@stu.d214.org>
2023-04-06 15:03:00 -0500

    fixed bindgind in navigation link

--- VerbView.swift
+++ VerbView.swift
@@ -10,7 +10,7 @@ struct VerbView: View {
     @Binding var myMadLib:MadLib
     var body: some View {
     
-        NavigationLink("AdjectiveView go", destination: AdjectiveView())
+        NavigationLink("AdjectiveView go", destination: AdjectiveView(myMadLib: $myMadLib))
         TextField("hi", text: $myMadLib.verb)
         
         
